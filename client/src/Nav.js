7a8e0742cea75d2fa47d10ece69975953f3f6cfe
Sharon Z <xiaozeng111@gmail.com>
2020-09-19 11:47:38 +1000

    Make website mobile responsive

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -5,10 +5,13 @@ import {
 import style from './nav.module.css';
 
 export const Nav = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleMenuOpen = () => setOpen(!open);
 
-    return (<nav className={style.nav}>
-            <div className={style.logo}></div>
-            <ul>
+    return (
+        <nav className={style.nav}>
+            <Link to="/" className={style.logo} />
+            <ul className={style.desktop}>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
@@ -22,6 +25,25 @@ export const Nav = () => {
                     <Link to="/contact">Contact</Link>
                 </li>
             </ul>
+
+            <div className={style.mobile}>
+                <span className={style.hamburger} onClick={handleMenuOpen}/>
+                {open && <ul>
+                    <li>
+                        <Link to="/" onClick={handleMenuOpen}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about" onClick={handleMenuOpen}>About</Link>
+                    </li>
+                    <li>
+                        <Link to="/projects" onClick={handleMenuOpen}>Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" onClick={handleMenuOpen}>Contact</Link>
+                    </li>
+                </ul>}
+            </div>
         </nav>
+
     );
 };
